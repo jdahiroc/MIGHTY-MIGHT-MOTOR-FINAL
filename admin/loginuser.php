e03f444953da755588f3bea9af3493110609841d
jdahiroc <jdahiroc_210000000608@uic.edu.ph>
2023-05-30 06:45:47 +0800

    first commit

--- admin/loginuser.php
+++ admin/loginuser.php
@@ -1,3 +1,30 @@
+<?php
+$username = $_POST['username'];
+$passwords = $_POST['passwords'];
+
+//Database connection
+$con = new mysqli("localhost", "root", "", "mightymightmotor");
+if (isset($_POST['submit'])) {
+    if ($con->connect_error) {
+        die("Failed to connect : " . $con->connect_error);
+    } else {
+        $stmt = $con->prepare("select * from user where username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+        if ($stmt_result->num_rows > 0) {
+            $data = $stmt_result->fetch_assoc();
+            if ($data['password'] === $passwords) {
+                echo "<script>alert('Login Successfully!');</script>";
+                echo "<script>window.location.href = '/MIGHTY-MIIGHT-MOTOR/admin/index.php'";
+            } else {
+                echo "<script>alert('Invalid Email or Password');</script>";
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -27,15 +54,14 @@
                 <h2 class="d-flex justify-content-center">Login</h2>
                 <div class="pt-2 g-col-6">
                     <label>Username</label>
-                    <input type="text" name="user_username" id="user_username" class="form-control" ;>
+                    <input type="text" name="username" id="username" class="form-control" ;>
                 </div>
                 <div class="pt-2 g-col-6">
                     <label>Password</label>
-                    <input type="password" name="user_password" id="user_password" class="form-control">
+                    <input type="password" name="passwords" id="passwords" class="form-control">
                 </div>
                 <div class="form-group mt-3 mb-3 d-flex justify-content-center">
-                    <input type="submit" class="btn btn-primary" value="Login" id="user_login" name="user_login" ;
-                        href="/MIGHTY-MIIGHT-MOTOR/admin/index.php">
+                    <input type="submit" class="btn btn-primary" value="Login" id="submit" name="submit" ;>
                 </div>
                 <p>Don't have an account? <a href="/MIGHTY-MIIGHT-MOTOR/admin/CreateUserInterface.php">Create an
                         Account.</a></p>
@@ -44,79 +70,3 @@
     </section>
 
 </html>
-
-<!--================================================================================================ -->
-<!-- CODE FOR LOGIN SYSTEM -->
-<?php
-// // Step 1: Establish database connection
-// $hostname = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "mightymightmotor";
-
-// $conn = mysqli_connect($hostname, $username, $password, $database);
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-// // Step 2: Retrieve user input
-// $username = $_POST['userName'];
-// $password = $_POST['passWord'];
-
-// // Step 3: Query the database
-// $query = "SELECT * FROM user WHERE username = ?";
-// $stmt = mysqli_prepare($conn, $query);
-// mysqli_stmt_bind_param($stmt, "s", $username);
-// mysqli_stmt_execute($stmt);
-
-// // Step 4: Execute the query
-// $result = mysqli_stmt_get_result($stmt);
-
-// $storedUsername = null;
-// $storedPassword = null;
-
-// // Step 5: Fetch the results
-// if ($row = mysqli_fetch_assoc($result)) {
-//     $storedUsername = $row['username'];
-//     $storedPassword = $row['password'];
-
-//     // Step 6: Validate and authenticate
-//     if ($password === $storedPassword) {
-//         // Credentials match, user is authenticated
-//         echo "Login successful!";
-//     } else {
-//         // Invalid password
-//         echo "Invalid password!";
-//     }
-// } else {
-//     // User not found
-//     echo "User not found!";
-// }
-
-// // Close the database connection
-// mysqli_close($conn);
-
-
-include('../admin/connect.php');
-
-if (isset($_POST['user_login'])) {
-    $user_username = $_POST['user_username'];
-    $user_password = $_POST['user_password'];
-
-    $select_query = "Select * from `user` where username='$user_username'";
-    $result = mysqli_query($conn, $select_query);
-    $row_count = mysqli_num_rows($result);
-    $row_data = mysqli_fetch_assoc($result);
-    if ($row_count > 0) {
-        if (password_verify($user_password, $row_data['user_password'])) {
-            echo "<script>alert('Login Successfully!')</script>";
-            header('header:/MIGHTY-MIIGHT-MOTOR/admin/index.php');
-        } else {
-            echo "<script>alert('Malii Credentials')</script>";
-        }
-    } else {
-        echo "<script>alert('Invalid Credentials')</script>";
-    }
-
-}
-?>
